8c5051d289dd50ea8777b5b2e43abb22f54a26d8
Juan Millord <jmillord@ambiente.gob.do>
2020-02-24 14:49:37 -0400

    added animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "../index.css";
 import { Link } from "react-router-dom";
+import { useSpring, animated } from "react-spring";
 
 function Home() {
+  const fade = useSpring({
+    from: {
+      opacity: 0
+    },
+
+    opacity: 1
+  });
   return (
-    <div className="container">
+    <animated.div className="container" style={fade}>
       <h1 className="title">Geo-Protestas</h1>
       <br />
       <p id="intro">
@@ -14,7 +22,7 @@ function Home() {
         de distribución de los lugares donde se ha protesado desde el 16 de
         febrero 2020.
       </p>
-    </div>
+    </animated.div>
   );
 }
 
